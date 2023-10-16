c2837184e8c4dc87472b5e718b2ef97995c91134
David <davidbish2002@hotmail.com>
2023-10-15 21:36:49 -0230

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,12 @@
 import Gate from "@components/gate";
-import Home from "@views/home";
 import Login from "@views/login";
 import Register from "@views/register";
 import Profile from "@views/profile";
-import Raffle from "@views/raffle";
-import Sponsors from "@views/sponsors";
 import PasswordReset from "@views/password/reset";
 
 export default [
   {
-    path: "/login",
+    path: "/",
     element: <Gate view={<Login />} restricted={0} />,
   },
   {
@@ -21,19 +18,7 @@ export default [
     element: <Gate view={<PasswordReset />} restricted={0} />,
   },
   {
-    path: "/",
-    element: <Gate view={<Home />} restricted={1} />,
-  },
-  {
     path: "/profile",
     element: <Gate view={<Profile />} restricted={1} />,
-  },
-  {
-    path: "/raffle",
-    element: <Gate view={<Raffle />} restricted={1} />,
-  },
-  {
-    path: "/sponsors",
-    element: <Gate view={<Sponsors />} restricted={1} />,
   },
 ];
